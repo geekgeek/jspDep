1c139486418686b681d7b7f435633c5084cf4497
geekgeek <solq87@gmail.com>
2017-01-03 19:31:31 +0100

    Add files via upload

---
+++ WEB-INF/classes/package1/Boat.java
@@ -0,0 +1,20 @@
+package package1;
+
+public class Boat
+{
+public String name;
+
+public Boat(String name)
+{
+this.name = name;
+}
+
+public String toString()
+{
+String info = "";
+info+= "Boat ";
+info+= name;
+return info;
+}
+
+}
---
+++ WEB-INF/classes/package1/Car.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Car
+{
+public Car()
+{
+
+}
+public String print()
+{
+return "I am car";
+}
+
+
+}
